474208ed53e2b1b9219d6224382d25a7658686fc
bgh2909 <bgh2909@gmail.com>
2024-01-29 04:39:58 +0700

    Add project files.

---
+++ BT_KiemTraSoNguyenTo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_KiemTraSoNguyenTo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Number: ");
+            int N = int.Parse(Console.ReadLine());
+            
+            if (N < 2)
+            {
+                Console.WriteLine(N + " is not a prime");
+            }
+            
+            else
+            {
+                int i = 2;
+                bool check = true;
+                
+                while (i <= Math.Sqrt(N))
+                {
+                    if (N%i==0)
+                    { 
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                {
+                    Console.WriteLine(N + " is a prime");
+                }
+                else
+                {
+                    Console.WriteLine(N + " is not a prime");
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
